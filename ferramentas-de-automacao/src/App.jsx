92635d21c8afb911d5fc5dcceed5ac7a403c9097
Pavanelli7 <vittor.ribeiro@sptech.school>
2023-07-06 14:13:28 -0300

    Tópico/Componentes

--- ferramentas-de-automacao/src/App.jsx
+++ ferramentas-de-automacao/src/App.jsx
@@ -1,15 +1,24 @@
 import React from "react";
+import Header from "./Header"
+import Footer from "./Footer";
+import Form from "./Form/Form";
+
+const Teste = () => {
+  const active = true;
+  if (active) {
+    return <p>Teste</p>
+  } else {
+    return null;
+  }
+}
 
 const App = () => {
-  function handleScroll(event) {
-    console.log(event);
-  }
-  window.addEventListener('scroll', handleScroll);
   return (
     <>
-      <div style={{height: '200vh'}}>
-        <button onClick={(event) => alert(event.target.innerText)}>Compre Violão</button>
-      </div>
+        <Teste />
+        <Header />
+        <Form />
+        <Footer />
     </>
   )
 };
