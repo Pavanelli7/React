b68162e5de010f9fa24a102144735e473077a602
Pavanelli7 <vittor.ribeiro@sptech.school>
2023-07-12 14:15:15 -0300

    Tópico/form/Select

--- ferramentas-de-automacao/src/App.jsx
+++ ferramentas-de-automacao/src/App.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 
 const App = () => {
-  const [textarea, setTextarea] = React.useState('');
+  const [select, setSelect] = React.useState('');
 
   return (
     <>
       <form>
-        <label htmlFor="nome">Nome</label>
-        <textarea
-          id="nome"
-          value={textarea}
-          rows={"5"}
-          onChange={({ target }) => setTextarea(target.value)}
-        />
-        {textarea}
+        <select value={select} id="produtos" onChange={({ target }) => setSelect(target.value)}>
+          <option disabled value="">Selecione</option>
+          <option value="notebook">Notebook</option>
+          <option value="smartphone">Smartphone</option>
+          <option value="tablet">Tablet</option>
+        </select>
+        {select}
       </form>
     </>
   );
