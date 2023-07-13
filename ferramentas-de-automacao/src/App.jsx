a6be0bfb8dd8e7cabcd3cbcda3aa9ad84b286589
Pavanelli7 <vittor.ribeiro@sptech.school>
2023-07-13 10:37:59 -0300

    Tópico/form/Componentes Checkbox

--- ferramentas-de-automacao/src/App.jsx
+++ ferramentas-de-automacao/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Input from "./Form/Input";
 import Select from "./Form/Select";
 import Radio from "./Form/Radio";
+import Checkbox from "./Form/Checkbox";
 
 const App = () => {
   const [nome, setNome] = React.useState('');
@@ -9,9 +10,15 @@ const App = () => {
   const [produto, setProduto] = React.useState('');
   const [cor, setCor] = React.useState('Vermelho');
   const [frutas, setFrutas] = React.useState('');
+  const [linguagens, setLinguagens] = React.useState([]);
+  const [termos, setTermos] = React.useState([]);
 
   return (
     <form>
+      <h2>termos</h2>
+      <Checkbox options={['Li e aceito os termos.']} value={termos} setValue={setTermos} />
+      <h2>Checkbox</h2>
+      <Checkbox options={['JavaScript', 'PHP', 'Ruby']} value={linguagens} setValue={setLinguagens} />
       <h2>Frutas</h2>
       <Radio options={['Maçã', 'Uva']} value={frutas} setValue={setFrutas} />
       <h2>Cores</h2>
