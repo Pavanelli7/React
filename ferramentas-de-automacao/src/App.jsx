3a386a8ebac8108908f1503025726a1d5a4dc8cc
Pavanelli7 <vittor.ribeiro@sptech.school>
2023-07-14 14:15:14 -0300

    Tópico/estado

--- ferramentas-de-automacao/src/App.jsx
+++ ferramentas-de-automacao/src/App.jsx
@@ -1,29 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Preco = styled.p`
-  background: ${(props) => props.cor};
-  color: white;
-  display: inline-block;
-  border-radius: 5px;
+const Button = styled.button`
+  background: ${({ ativo }) => (ativo ? '#53d956' : '#000')};
+  border: 1px solid black;
+  font-size: 1rem;
   padding: 0.5rem;
+  border-radius: 4px;
+  color: white;
+  cursor: pointer;
 `;
 
 const App = () => {
-  function template(value, total) {
-    console.log(value);
-    console.log(total);
-  }
-  const total = 10;
-  template`São ${total} no total`;
+  const [ativo, setAtivo] = React.useState(false);
 
   return (
-    <div>
-      <Preco cor="#53d956">R$ 2999</Preco>
-      <Preco cor="#84e">R$ 1999</Preco>
-    </div>
+    <Button ativo={ativo} onClick={() => setAtivo(!ativo)}>
+      Ativar
+    </Button>
   );
 };
-
 
 export default App;
