5eb275e426abd461e578b8c853256b876660e0d7
Pavanelli7 <vittor.ribeiro@sptech.school>
2023-07-20 14:59:44 -0300

    Tópico/PropTypes

---
+++ ferramentas-de-automacao/src/Button.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = (props) => {
+  return (
+    <button
+      style={{
+        margin: props.margin,
+        width: `${props.width}px`,
+        height: `${props.width / 3}px`,
+      }}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+Button.defaultProps = {
+  width: 200,
+  disabled: false,
+};
+
+Button.propTypes = {
+  margin: PropTypes.string.isRequired,
+  width: PropTypes.number,
+  disabled: PropTypes.bool,
+};
+
+export default Button;
